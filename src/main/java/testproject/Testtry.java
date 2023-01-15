963fe5dbb366ed8db406ac960fff5c01971d1651
Ragini <rags.cool16@gmail.com>
2023-01-15 21:11:04 +0530

    new test methods added

--- src/main/java/testproject/Testtry.java
+++ src/main/java/testproject/Testtry.java
@@ -10,6 +10,22 @@ public void login()
 {
 System.out.println("hello");
 }
+
+@Test
+public void login1()
+{
+System.out.println("hello1");
+}
+@Test
+public void login2()
+{
+System.out.println("hello2");
+}
+@Test
+public void login3()
+{
+System.out.println("hello3");
+}
 	
 
 }
